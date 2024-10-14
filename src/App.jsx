6ff084162dd60863724d22c75fafde54b9baf242
Ranatarek22 <rt589611@gmail.com>
2024-10-14 22:49:18 +0300

    fixing routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,70 @@
-// In App.js
+import { useEffect, useState } from "react";
+import LoginForm from "./components/forms/loginForm";
+import Missing from "./components/Missing";
+import Unauthorized from "./components/Unautharized";
+import LinkPage from "./components/LinkPage";
+import SignUpForm from "./components/forms/registerForm";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import RequireAuth from "./components/RequiredAuth";
+import Home from "./components/pages/Home";
+import Driver from "./components/pages/Users/Driver";
+import Maintainer from "./components/pages/Users/Maintainer";
+import BlockRoutes from "./components/routes/blockRoutes";
+import useRefreshToken from "./hooks/useRefreshToken";
+import useAuth from "./hooks/useAuth";
+import { AppointmentProvider } from "./components/context/AppointmentProvider";
 
-<Routes>
-  <Route path="/" element={<Layout />}>
-    {/* Public Routes */}
-    <Route
-      path="login"
-      element={
-        <BlockRoutes>
-          <LoginForm />
-        </BlockRoutes>
-      }
-    />
-    <Route
-      path="register"
-      element={
-        <BlockRoutes>
-          <SignUpForm />
-        </BlockRoutes>
-      }
-    />
-    <Route path="linkpage" element={<LinkPage />} />
-    <Route path="unauthorized" element={<Unauthorized />} />
-    <Route path="/" index element={<Home />} />
+function App() {
+  const { auth, setAuth } = useAuth();
+  const refresh = useRefreshToken();
 
-    {/* Protected Routes - Role-based access */}
-    <Route element={<RequireAuth allowedRoles={["driver"]} />}>
-      <Route path="driver" element={<Driver />} />
-    </Route>
+  return (
+    // <Maintainer />
 
-    <Route element={<RequireAuth allowedRoles={["maintainer"]} />}>
-      <Route
-        path="maintainer"
-        element={
-          <AppointmentProvider>
-            <Maintainer />
-          </AppointmentProvider>
-        }
-      />
-    </Route>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* Public Routes */}
+        <Route
+          path="login"
+          element={
+            <BlockRoutes>
+              <LoginForm />
+            </BlockRoutes>
+          }
+        />
+        <Route
+          path="register"
+          element={
+            <BlockRoutes>
+              <SignUpForm />
+            </BlockRoutes>
+          }
+        />
+        <Route path="linkpage" element={<LinkPage />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
+        <Route path="/" index element={<Home />} />
 
-    <Route path="*" element={<Missing />} />
-  </Route>
-</Routes>;
+        {/* Protected Routes - Role-based access */}
+        <Route element={<RequireAuth allowedRoles={["driver"]} />}>
+          <Route path="driver" element={<Driver />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={["maintainer"]} />}>
+          <Route
+            path="maintainer"
+            element={
+              <AppointmentProvider>
+                <Maintainer />
+              </AppointmentProvider>
+            }
+          />
+        </Route>
+
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
