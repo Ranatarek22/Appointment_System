e8934317fc02b7d1d86b0bf633f562dc1977b0ff
Ranatarek22 <rt589611@gmail.com>
2024-10-20 03:52:04 +0300

    Services Card fixed

--- src/components/pages/HomePage/Services.jsx
+++ src/components/pages/HomePage/Services.jsx
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from "react";
 import { apiInstance } from "../../../axios";
 import ServiceCard from "./ServiceCard";
 import { useTranslation } from "react-i18next";
+import { IconButton } from "@mui/material";
+import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
 
 export default function Services() {
   const [maintainers, setMaintainers] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0); 
   const { t } = useTranslation();
 
   useEffect(() => {
@@ -22,6 +25,20 @@ export default function Services() {
     fetchMaintainers();
   }, []);
 
+  const handleNext = () => {
+    if (currentIndex + 3 < maintainers.length) {
+      setCurrentIndex((prevIndex) => prevIndex + 3);
+    }
+  };
+
+  const handlePrev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex((prevIndex) => prevIndex - 3);
+    }
+  };
+
+  const visibleMaintainers = maintainers.slice(currentIndex, currentIndex + 3);
+
   return (
     <div
       className="container-fluid"
@@ -32,25 +49,36 @@ export default function Services() {
     >
       <section id="services" className="services">
         <h3
-          className="mb-5"
+          className="mb-5 text-[2rem] md:text-[4rem] font-bold"
           style={{
-            fontSize: "2rem",
+        
             fontWeight: "40px",
             letterSpacing: "-2px",
-            textAlign:'center',
+            textAlign: "center",
             color: "var(--new-color)",
           }}
         >
           {t("company_mant")}
         </h3>
-        <div className="cards flex-col md:flex-row w-full">
-          {maintainers.map((maintainer, index) => (
-            <ServiceCard
-              key={maintainer.id}
-              title={maintainer.name}
-              description={t("company_description")}
-            />
-          ))}
+        <div className="flex justify-between items-center">
+          <IconButton onClick={handlePrev} disabled={currentIndex === 0}>
+            <ArrowBackIos />
+          </IconButton>
+          <div className="cards flex-col md:flex-row w-full justify-center">
+            {visibleMaintainers.map((maintainer) => (
+              <ServiceCard
+                key={maintainer.id}
+                title={maintainer.name}
+                description={t("company_description")}
+              />
+            ))}
+          </div>
+          <IconButton
+            onClick={handleNext}
+            disabled={currentIndex + 3 >= maintainers.length}
+          >
+            <ArrowForwardIos />
+          </IconButton>
         </div>
       </section>
     </div>
